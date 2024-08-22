c6d57359c303f52dddef61703bbe5f37838f6fee
itserror808 <meftahahmedreda02@gmail.com>
2024-08-22 15:54:52 +0100

    docker

--- src/main/java/com/example/portfolioback/config/WebConfig.java
+++ src/main/java/com/example/portfolioback/config/WebConfig.java
@@ -1,15 +1,20 @@
 package com.example.portfolioback.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${ALLOWED_ORIGIN}")
+    private String allowedOrigin;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000") // Replace with your frontend URL
+                .allowedOrigins(allowedOrigin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
